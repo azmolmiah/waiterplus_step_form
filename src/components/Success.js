07e440f91327e04ad7ffe04ec840c0038c2b869a
Azmol-Miah <azmol@azmolmiah.com>
2020-04-19 12:46:44 +0100

    Added appbar and home button to succes component

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,13 +1,23 @@
 import React, { Fragment } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 
 const Success = () => {
   return (
     <Fragment>
       <Dialog open={true} fullWidth={true} maxWidth="sm">
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center", margin: "1rem" }}>
+          <AppBar position="static">
+            <h4>Waiterplus</h4>
+          </AppBar>
+          <br />
           <h3>Thank You For Your Submission</h3>
           <p>You will get an email with further instructions</p>
+          <br />
+          <Button color="primary" variant="contained">
+            Home
+          </Button>
         </div>
       </Dialog>
     </Fragment>
